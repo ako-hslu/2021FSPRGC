28ddb433f5c0270af554fdcdf600851f0bf477d1
Adrian Koller <adrian.koller@hslu.ch>
2021-04-22 20:25:37 +0200

    cleanup

--- 21_Workshops/WSPrograms/WS06.3_CircleAreaByReference.c
+++ 21_Workshops/WSPrograms/WS06.3_CircleAreaByReference.c
@@ -2,7 +2,11 @@
 
 // function declaration!
 // float* area means we are passing the address of the variable area
-void ComputeCircleArea(float radius, float* area);
+void ComputeCircleAreaByReference(float radius, float *area);
+
+float ComputeCircleAreaByValue(float radius);
+
+void UpdateStudent( struct Student *pStud );
 
 
 int main()
@@ -10,7 +14,10 @@ int main()
 	float myRadius = 4.545;
 	float area = 0.0f;
 	// pass the address of the area using '&'
-	ComputeCircleArea(myRadius, &area);
+	ComputeCircleAreaByReference(myRadius, &area);
+	printf("Given radius %.3f, the area equals %.3f\n", myRadius, area);
+	
+	area = ComputeCircleAreaByValue(myRadius); // pass by value
 	printf("Given radius %.3f, the area equals %.3f\n", myRadius, area);
 	return 0;
 }
@@ -18,6 +25,15 @@ int main()
 // function implementation
 void ComputeCircleArea(float radius, float* area)
 {
-	// *area means we are modifying the value at the address 'area'
+ 	// *area means we are modifying the value at the address 'area'
 	*area = radius*radius*3.1415926535;
 }
+
+
+
+float ComputeCircleAreaByValue(float radius ){
+	float pi = 3.1415927;
+	float area = radius*radius*pi;
+	return area;
+}
+
--- 21_Workshops/WSPrograms/WS07_all_students.c
+++ 21_Workshops/WSPrograms/WS07_all_students.c
@@ -21,3 +21,39 @@ struct Student students[NUM_STUDENTS] = {
 [4].firstname = "Natasha", 	[4].lastname = "Romanoff", 	[4].grades = {5.3, 5.2, 5.1, 5.7, 4.9, 5.9} };
 
 
+void PrintStudent( struct Student *student )
+{	
+	printf("%s %s, Grades: ", student->firstname, student->lastname);
+	for(int i = 0; i < NUM_GRADES; i++ )
+	{
+		printf("%0.1f ", student->grades[i]);
+	}
+	printf("- Average: %0.2f\n", student->average);
+	
+}
+
+void ComputeStudentGradeAverage( struct Student *student )
+{
+	float tempAverage = 0.0f;
+	for( int i = 0; i < NUM_GRADES; i++ )
+	{
+		tempAverage = tempAverage + student->grades[i];
+	}
+	student->average = tempAverage/NUM_GRADES;
+}
+
+
+int main(int argc, char *argv[] )
+{
+	float classAverage = 0.0;
+	for( int i = 0; i < NUM_STUDENTS; i++ ){
+		ComputeStudentGradeAverage(&students[i]);
+		PrintStudent(&students[i]);
+		classAverage = classAverage + students[i].average;
+	}
+	classAverage = classAverage/NUM_STUDENTS;
+	
+	printf("\n\nClass average grade: %0.2f\n", classAverage);
+	
+	return 0;
+}
