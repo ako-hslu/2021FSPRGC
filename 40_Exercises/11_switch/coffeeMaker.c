c486816050913f240b8857307303f850d5096bd2
Adrian Koller <adrian.koller@hslu.ch>
2021-04-29 16:45:35 +0200

    coffee maker

---
+++ 40_Exercises/11_switch/coffeeMaker.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <unistd.h>
+
+typedef enum {
+	HEATING = 1,
+	READY,
+	MILLING,
+	COFFEEMAKING,
+	CLEARING,
+	SHUTDOWN,
+	ERROR
+} CoffeeMakerStatus;
+
+CoffeeMakerStatus status = HEATING;
+	
+int HeatingDone(void);
+
+
+int main(void) {
+
+	// endless loop
+	int run = 1;
+	while(run == 1) 
+	{
+		
+		switch(status)
+		{
+			case HEATING:
+				if( HeatingDone() )
+				{
+					status = SHUTDOWN;
+				}
+				break;
+		
+		
+			// add all cases
+		
+			case SHUTDOWN:
+				printf("Shutting down.\n");
+				run = 0;
+				break;
+
+
+			default:
+				printf("Coffee Maker in Error State.\n");
+				status = SHUTDOWN;
+				break;
+		}
+	}
+
+	return 0;
+}
+
+
+static int heatingCounter = 0;
+int heatingLimit = 6;
+
+int HeatingDone(void){
+	int done = 0;
+	sleep(1);
+	printf("#");
+	fflush(stdout);
+	heatingCounter++;
+	if(heatingCounter >= heatingLimit)
+	{
+		done = 1;
+		printf(" Heating Completed.\n");
+	}
+	return done;
+}
+	
+
+
+
+
