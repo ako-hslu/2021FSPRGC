7fd8bbb4d4054fa26961080179ceaa4c3200d1dc
Adrian Koller <adrian.koller@hslu.ch>
2021-05-06 17:34:03 +0200

    modular

---
+++ 40_Exercises/08_modular/greetings.c
@@ -0,0 +1,41 @@
+/* system includes */
+#include <stdio.h>
+
+
+/* project includes */
+#include "greetings.h"
+
+
+struct Prof {
+	char name[12];
+	int salary;
+}
+
+
+
+/* globale variable: declaration + init */
+int greetingsCounter = 0;
+
+/* local variable */
+int localCounter = 24;
+
+/* private function */
+void IncrementGreetingsCounter( void ) {
+	greetingsCounter++;
+}
+
+
+/* implementations */
+void SayHello(void) {
+	printf("Hello!\n");
+	IncrementGreetingsCounter();
+}
+
+
+
+void SayGoodBye(void) {
+	printf("Goodbye!!\n");
+	IncrementGreetingsCounter();
+}
+
+
---
+++ 40_Exercises/08_modular/main.c
@@ -0,0 +1,22 @@
+#include "greetings.h"
+
+
+struct Student myStud;
+struct Prof myProf;
+
+
+int main( void ) {
+	
+	while( greetingsCounter < 10 ){
+		SayHello();
+		SayGoodBye();
+
+	}
+
+	return 0;
+}
+
+
+
+
+
